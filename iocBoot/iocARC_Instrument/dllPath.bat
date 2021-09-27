366119bae7756d43ee87db8ba0e7a96d4c3e6ea7
Heesterman, Peter J <Peter.Heesterman@ukaea.uk>
2021-09-27 10:56:32 +0100

    ARC_Instrument is a Windows-only IOC because it depends on Acton's DLL.

--- iocBoot/iocARC_Instrument/dllPath.bat
+++ iocBoot/iocARC_Instrument/dllPath.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-SET "PATH=C:/EPICS/iocTops/PrincetonSpectro/bin/win32-x86-static;\epics/support/asyn/bin/win32-x86-static;\epics/support/stream/bin/win32-x86-static;\epics/base/bin/win32-x86-static;%PATH%"
+SET "PATH=D:/epics/iocTops/PrincetonSpectro/bin/win32-x86-static;\epics/support/asyn/bin/win32-x86-static;\epics/support/stream/bin/win32-x86-static;\epics/base/bin/win32-x86-static;%PATH%"
