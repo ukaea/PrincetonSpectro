fbb8198711eea7d67d37d502ebe1479ddc0692d3
Heesterman, Peter J <Peter.Heesterman@ukaea.uk>
2022-08-08 14:39:40 +0100

    Is 'BASE_RECUSIVE' relevant?

---
+++ .ci-local/princetonspectro_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+# This script installs NDS and NDS-epics
+#
+# check if user has right permissions
+if [ "$(id -u)" != "0" ]; then
+    echo "Sorry, you are not root. Please try again using sudo."
+    exit 1
+fi
+
+# terminate script after first line that fails
+set -e
+
+apt-get install -y libreadline6-dev libncurses5-dev perl clang g++-mingw-w64-i686 g++-mingw-w64-x86-64 qemu-system-x86 re2c tar
+apt-get install -y build-essential git python curl p7zip-full wget libxml2-dev
+
+export GITLAB_CI=1
+export SETUP_PATH=".ci-local:.ci"
+export BASE_RECURSIVE="YES"
+export CMP="gcc"
+export BGFC="static"
+export SET="stable"
+export CLEAN_DEPS="NO"
+export WINE=64
+export VV=1
+export CLEAN_DEPS="NO"
+echo "PREPARE" > /tmp/princetonspectro_install.log
+python .ci/cue.py prepare > /tmp/princetonspectro_install.log 2>&1
+echo "BUILD" > /tmp/princetonspectro_install.log
+python .ci/cue.py build > /tmp/princetonspectro_install.log 2>&1
+
