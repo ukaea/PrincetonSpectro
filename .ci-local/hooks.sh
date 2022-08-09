935b53e1f1f058719344cc4d980d07792384c239
Heesterman, Peter J <Peter.Heesterman@ukaea.uk>
2022-08-09 18:27:25 +0100

    I wonder where mingw is installed?

--- .ci-local/hooks.sh
+++ .ci-local/hooks.sh
@@ -14,7 +14,7 @@ set -e
 # These files are created by the NDS build, wheras Linux builds install to a system folder.
 if [ "$WINE" == "64" ]; then
     # ls /usr/lib/gcc/x86_64-w64-mingw32
-    find /usr -name '*mingw*'
+    find -name '*mingw*'
     # see https://github.com/randombit/botan/issues/2039
     if [ ! -f /usr/bin/x86_64-w64-mingw32-g++-win32 ]; then
         apt-get install -y g++-mingw-w64-x86-64
